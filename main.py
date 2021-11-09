3649849f3905189861fd1c32ce6b846391af4689
tonyhart7 <tonyhart2020@gmail.com>
2021-11-09 22:45:31 +0700

    .

--- main.py
+++ main.py
@@ -40,3 +40,5 @@ def get_post(id: int):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return {"post_detail": post}
+
+
